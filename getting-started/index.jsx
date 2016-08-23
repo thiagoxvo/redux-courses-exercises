ca6abfacc285b61855c7264ad9daf22bef46db66
Thiago Xavier <thiagoxvo@gmail.com>
2016-08-23 19:46:04 -0300

    avoiding array mutations with concat slice and spread

--- getting-started/index.jsx
+++ getting-started/index.jsx
@@ -1,47 +1,59 @@
-// import expect from 'expect'
-import { createStore } from 'redux'
-import React from 'react'
-import ReactDOM from 'react-dom'
+import expect from 'expect'
+import deepFreeze from 'deep-freeze'
 
-const counter = (state = 0, action) => {
-  switch (action.type){
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    default:
-      return state
-  }
+const addCounter = (list) => {
+  return [...list, 0]
 }
 
-const Counter = ({ value, onIncrement, onDecrement }) => (
-  <div>
-    <h1>{value}</h1>
-    <button onClick={onIncrement}>+</button>
-    <button onClick={onDecrement}>-</button>
-  </div>
-)
-
-const store = createStore(counter)
-
-const render = () => {
-  ReactDOM.render(
-    <Counter
-      value={store.getState()}
-      onIncrement={() =>
-        store.dispatch({
-          type: 'INCREMENT'
-        })
-      }
-      onDecrement={() =>
-        store.dispatch({
-          type: 'DECREMENT'
-        })
-      }
-    />,
-    document.getElementById('app')
-  )
+const removeCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    ...list.slice(index + 1)
+  ]
 }
 
-store.subscribe(render)
-render()
+const incrementCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    list[index] + 1,
+    ...list.slice(index + 1)
+  ]
+}
+
+const testAddCounter = () => {
+  const listBefore = []
+  const listAfter = [0]
+
+  deepFreeze(listBefore)
+  expect(
+    addCounter(listBefore)
+  ).toEqual(listAfter)
+}
+
+const testRemoveCounter = () => {
+  const listBefore = [0, 10, 20]
+  const listAfter = [0, 20]
+
+  deepFreeze(listBefore)
+
+  expect(
+    removeCounter(listBefore, 1)
+  ).toEqual(listAfter)
+}
+
+const testIncrementCounter = () => {
+  const listBefore = [0, 10, 20]
+  const listAfter = [0, 11, 20]
+
+  deepFreeze(listBefore)
+
+  expect(
+    incrementCounter(listBefore, 1)
+  ).toEqual(listAfter)
+}
+
+
+testAddCounter()
+testRemoveCounter()
+testIncrementCounter()
+console.log('All tests passed')
