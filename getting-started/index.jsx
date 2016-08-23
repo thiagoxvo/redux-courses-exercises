2d9590fee5edc7eb3f0090c99efedaa344165337
Thiago Xavier <thiagoxvo@gmail.com>
2016-08-23 15:23:05 -0300

    writing a counter redux with tests

--- getting-started/index.jsx
+++ getting-started/index.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import expect from 'expect'
 
-export class App extends React.Component {
-	render() {
-		return (
-			<div>Hello World</div>
-		)
-	}
+const counter = (state = 0, action) => {
+  switch (action.type){
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
 }
 
-ReactDOM.render(<App/>, document.querySelector("#app"))
+expect(
+  counter(0, { type: 'INCREMENT'})
+).toEqual(1)
+
+expect(
+  counter(1, { type: 'INCREMENT'})
+).toEqual(2)
+
+expect(
+  counter(2, { type: 'DECREMENT'})
+).toEqual(1)
+
+expect(
+  counter(1, { type: 'DECREMENT'})
+).toEqual(0)
+
+expect(
+  counter(0, { type: 'SOMETHING_ELSE'})
+).toEqual(0)
+
+expect(
+  counter(undefined, {})
+).toEqual(0)
+
+console.log('Tests are passing!')
