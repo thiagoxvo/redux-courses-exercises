6b3f7b2fbde6a0f7f95ae20c025b8423b8f9d1f3
Thiago Xavier <thiagoxvo@gmail.com>
2016-08-24 18:44:13 -0300

    reducer composition with combineReducers()

--- getting-started/index.jsx
+++ getting-started/index.jsx
@@ -1,6 +1,6 @@
 import expect from 'expect'
 import deepFreeze from 'deep-freeze'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 
 const todo = (state, action) => {
   switch (action.type) {
@@ -48,18 +48,10 @@ const visibilityFilter = (
   }
 }
 
-const todoApp = (state = {}, action) => {
-  return {
-    todos: todos(
-      state.todos,
-      action
-    ),
-    visibilityFilter: visibilityFilter(
-      state.visibilityFilter,
-      action
-    )
-  }
-}
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+})
 const store = createStore(todoApp)
 console.log(store.getState())
 
